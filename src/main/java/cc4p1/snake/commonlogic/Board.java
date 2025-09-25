743d43d36a3def1ab77ea80496562dc022240538
Alkerg <albert.arg500@gmail.com>
2025-09-25 12:14:56 -0500

    feat: add initial setup for game common logic

---
+++ src/main/java/cc4p1/snake/commonlogic/Board.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cc4p1.snake.commonlogic;
+
+/**
+ *
+ * @author Albert
+ */
+public class Board {
+    public int width = 128;
+    public int height = 128;
+}
---
+++ src/main/java/cc4p1/snake/commonlogic/Snake.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cc4p1.snake.commonlogic;
+
+/**
+ *
+ * @author Albert
+ */
+public class Snake {
+    public int size = 3;
+}
